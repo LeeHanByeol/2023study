dea92437124b4264ea9705f9cf5be789b5f0c7c4
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-03-14 23:46:52 +0900

    [level 2] Title: 숫자의 표현, Time: 0.09 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,25 +3,14 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i = 1; i <= n; i++){
-            if(divide(n, i)){
+        //연속된 자연수들로 n을 표현하는 방법의 개수
+        // = n의 약수 중 홀수의 개수
+        for(int i = 1; i <= n; i+=2){   //i = 1,3,5...
+            if(n % i == 0){ //i가 n의 약수라면
                 answer++;
             }
         }
         
         return answer;
-    }
-    public boolean divide(int n, int start){
-        int sum = 0;
-        while(sum < n){
-            sum += start;
-            start++;
-        }
-        if(sum == n){
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
