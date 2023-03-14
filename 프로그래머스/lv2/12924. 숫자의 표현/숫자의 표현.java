4a27238a6e1a46581f2257e556edc953de03cafd
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-03-14 23:54:27 +0900

    Update 숫자의 표현.java
    증명
    https://gkalstn000.github.io/2021/01/21/%EC%88%AB%EC%9E%90%EC%9D%98-%ED%91%9C%ED%98%84/

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,10 +1,13 @@
+//정수론 중...
+//연속된 자연수들의 합으로 n을 표현하는 방법의 개수
+// = n의 약수 중 홀수의 개수
+
+
 class Solution {
     
     public int solution(int n) {
         int answer = 0;
         
-        //연속된 자연수들로 n을 표현하는 방법의 개수
-        // = n의 약수 중 홀수의 개수
         for(int i = 1; i <= n; i+=2){   //i = 1,3,5...
             if(n % i == 0){ //i가 n의 약수라면
                 answer++;
