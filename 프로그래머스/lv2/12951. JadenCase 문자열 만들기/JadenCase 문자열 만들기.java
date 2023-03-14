f427879b7d65c54ade6be71811bd0e4404de4e8c
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-03-14 11:40:52 +0900

    Update JadenCase 문자열 만들기.java

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,3 +1,13 @@
+//trim()을 썼다가 말았다. 공백으로 시작하거나 끝나는 문자열도 있는듯
+
+//공백이 여러개일 수 있다길래 String의 split(정규식) 사용하려 했는데, 그러면 공백을 저장할 수가 없었음.
+//따라서 구분자도 token으로 저장할 수 있는 StringTokenizer를 사용하였다.
+//split은 데이터를 잘라 String[]으로 반환하기에, 데이터를 토큰으로 바로바로 잘라서 반환하는 StringTokenizer보다 느리다.
+
+//문자열 변경이 필요했기에 StringBuffer 사용했음.
+//String으로 변경하면, 새 주소에 할당하고 참조변수가 가리키는 주소를 바꾸는 거라 좀 느릴 것 같아서...
+//연산 많은 것도 아닌데 크게 차이가 있겠냐만...
+
 import java.util.StringTokenizer;
 
 class Solution {
