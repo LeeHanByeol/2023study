65b3bb1e04fa9e7d8e5c0f2a3ea595faa33bebe9
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-03-22 22:09:36 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1306.72 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        
+        Set<Integer> num_list = new HashSet<>();
+        for(int len = 1; len <= elements.length; len++){
+            for(int start_idx = 0; start_idx < elements.length; start_idx++){
+                int sum = 0;
+                for(int i = 0; i < len; i++){
+                    sum += elements[(start_idx + i) % elements.length];
+                }
+                num_list.add(sum);
+            }
+        }
+        
+        return num_list.size();
+    }
+}
