21b3407437d7264e361856e37c46dd5117b87507
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-03-22 21:41:03 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int i = 0; i < progresses.length; ){
+            int max = (int)Math.ceil((double)(100-progresses[i]) / speeds[i]);
+            int count = 0;
+            do{
+                i++;
+                count++;
+            } while(i < progresses.length && max >= (int)Math.ceil((double)(100-progresses[i]) / speeds[i]));
+            
+            queue.add(count);
+        }
+        
+        int size = queue.size();
+        int[] answer = new int[size];
+        for(int i = 0; i < size; i++){
+            answer[i] = queue.poll();
+        }
+        return answer;
+    }
+}
