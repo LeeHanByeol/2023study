fc14daeaf345edaedb15b9d56b50b964f939e90e
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-03-28 17:10:15 +0900

    [level 2] Title: [3차] n진수 게임, Time: 198.00 ms, Memory: 335 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -3,30 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, int p) {
 
-        p -= 1;     //index로 접근할거라 1 빼줌
         String answer = "";
-        
-        int num = 0, len = 0;
-        char[] window = new char[m];
-        while(answer.length() < t){
-            String str = Integer.toString(num, n);  //n진법으로 변환
-            int leftSize = m - len;
-            if(leftSize > str.length()){   //다 들어갈 자리가 남아있다면
-                for(char a : str.toCharArray()){
-                    window[len++] = a;
-                }
+        String str = "";
+        for(int i = 0; i < t*m; i++){
+            str += Integer.toString(i, n);
+        }
+
+        int order = 1;        
+        for(int i = 0; i < str.length(); i++){
+            if(answer.length() == t){
+                break;
             }
-            else{   //자리가 남아있지 않다면
-                for(char a : str.substring(0, leftSize).toCharArray()){   //채울 수 있을 때까지 채운다
-                    window[len++] = a;
-                }
-                answer += window[p];
-                len = 0;                            //window 초기화하고
-                for(char a : str.substring(leftSize).toCharArray()){  //남은 글자 다시 채워줌
-                    window[len++] = a;
-                }
+            if(order > m){
+                order = 1;
             }
-            num++;            
+            if(order == p){
+                answer += str.charAt(i);
+            }
+            order++;
         }
         
         return answer.toUpperCase();
