ffe50e2d38ea17fa5841251c5bdc478cca8cf5d3
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-04-14 03:12:31 +0900

    re

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,21 +1,29 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-        
+
+        //모두 0인가 검증
+        boolean all_zero = true;
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] != 0){
+                all_zero = false;
+                break;
+            }
+        }
+        if(all_zero){
+            return "0";
+        }
+
+
         //String 배열로 바꿔준다
         String[] strArr = new String[numbers.length];
         for(int i = 0; i < numbers.length; i++){
             strArr[i] = Integer.toString(numbers[i]);
         }
-        
+
         //정렬
         Arrays.sort(strArr, (a, b) -> Integer.parseInt(b+a) - Integer.parseInt(a+b));
-        
-        //모두 0이라면
-        if(strArr[0].equals("0")){
-            return "0";
-        }
-        
+
         //string화
         return String.join("", strArr);
     }
