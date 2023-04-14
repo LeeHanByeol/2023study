23439450976d43d5dd12a9b0d1901179d47dccb1
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-04-14 14:59:20 +0900

    re

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,24 +1,25 @@
 import java.util.*;
 class Solution {
-    
+
     Set<Integer> checked = new HashSet<>();
-    
+
     public int solution(String numbers) {
         checked.add(0);
         checked.add(1);        
-        
+
         String[] arr = numbers.split("");
         boolean[] visited = new boolean[arr.length];
         return candidate(arr, "", visited);
     }
-    
+
     public int candidate(String[] arr, String cur, boolean[] visited){
-        
+
         int answer = 0;
+
         if(isPrime(cur)){
             answer++;
         }
-        
+
         for(int i = 0; i < arr.length; i++){
             if(!visited[i]){
                 visited[i] = true;
@@ -28,27 +29,23 @@ class Solution {
         }        
         return answer;
     }
-    
+
     public boolean isPrime(String number){
-        
+
         if(number.equals("")){
             return false;
         }
-        
-        int n = Integer.parseInt(number);
-        if(n==2 && !checked.contains(2)){
-            checked.add(2);
-            return true;
-        }
 
-        //기존에 구했던 수(중복) or 짝수
-        if(checked.contains(n) || (n&1) == 0){
+        int n = Integer.parseInt(number);
+
+        //기존에 구했던 수(중복)
+        if(checked.contains(n)){
             return false;
         }
-        
+
         //처음 구하는 수
         int end = (int)Math.sqrt(n);
-        for(int i = 3; i <= end; i+=2){
+        for(int i = 2; i <= end; i++){
             if(n % i == 0){
                 checked.add(n);
                 return false;
@@ -57,6 +54,6 @@ class Solution {
         checked.add(n);
         return true;
     }
-    
-    
+
+
 }
