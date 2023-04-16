230600e21112548231751f532ca687e3e4fc8278
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-04-16 22:20:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.41 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,37 +2,19 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         
-        StringBuilder stb = new StringBuilder(number);
+        int len = number.length() - k;      //잘라낸 후 문자열의 길이
         
-        //앞에 최대 k개가 있는 수 중, 가장 큰 수의 index(start) 골라내기
-        int max = 0;
-        int start = 0;
-        for(int i = 0; i < k; i++){
-            if(stb.charAt(i) > max){
-                max = stb.charAt(i);
-                start = i;
-            }
-        }
-        //start 앞의 수는 모두 제거
-        stb.delete(0, start);
-        k-= start;
+        Stack<Character> stk = new Stack<>();
         
-        //다음 숫자보다 작으면 제거
-        for(int i = 0; i < stb.length() - 1; i++){
-            if(k == 0){
-                break;
-            }
-            if(stb.charAt(i) < stb.charAt(i+1)){
-                stb.deleteCharAt(i);
+        for(char num : number.toCharArray()){
+            
+            while((k > 0) && (!stk.empty()) && (stk.peek() < num)){
+                stk.pop();
                 k--;
-                i -= 2;
             }
+            stk.push(num);            
         }
         
-        //뒤에서부터 제거
-        stb.delete(stb.length()-k, stb.length());
-
-        String answer = stb.toString();
-        return answer;
+        return stk.toString().replaceAll(", ", "").substring(1, len+1);    //,와 [] 잘라냄
     }
 }
