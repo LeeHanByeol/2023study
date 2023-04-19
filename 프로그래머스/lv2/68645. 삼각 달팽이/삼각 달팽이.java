e663a807df85d7fceeacdbbb568cb5f67b7b3349
LeeHanByeol <106506188+LeeHanByeol@users.noreply.github.com>
2023-04-19 22:00:18 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.47 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,35 +1,17 @@
 class Solution {
     public int[] solution(int n) {
 
-        
         int[][] triangle = new int[n][n];
-        int r = 0, c = 0;       //현재 위치
+        int r = -1, c = 0;       //현재 위치
         int num = 1;            //채워넣을 숫자
-        int action = 1;         //1->왼쪽 아래 2->오른쪽 3->왼쪽 위
+        int action = 0;         //0->왼쪽 아래 1->오른쪽 2->왼쪽 위
         for(int i = n; i > 0; i--){
-            switch(action){
-                case 1:                 //go down
-                    for(int j = 1; j < i; j++){
-                        triangle[r++][c] = num++;
-                    }
-                    triangle[r][c++] = num++;
-                    action++;
-                    break;
-                case 2:                 //go right
-                    for(int j = 1; j < i; j++){
-                        triangle[r][c++] = num++;
-                    }
-                    triangle[r--][c--] = num++;
-                    action++;
-                    break;
-                case 3:                 //go up
-                    for(int j = 1; j < i; j++){
-                        triangle[r--][c--] = num++;
-                    }
-                    triangle[r++][c] = num++;
-                    action = 1;
-                    break;
+            for(int j = 0; j < i; j++){
+                r = move_r(action, r);
+                c = move_c(action, c);
+                triangle[r][c] = num++;
             }
+            action = (++action) % 3;
         }
 
         //정답 채우기
@@ -42,6 +24,35 @@ class Solution {
         }
         
         return answer;
+    }
+
+    
+    public int move_r(int action, int r){
+        if(action == 0){
+            return ++r;
+        }
+        if(action == 1){
+            return r;
+        }
+        if(action == 2){
+            return --r;
+        }
+        System.out.println("error");
+        return -1;
+    }
+    
+    public int move_c(int action, int c){
+        if(action == 0){
+            return c;
+        }
+        if(action == 1){
+            return ++c;
+        }
+        if(action == 2){
+            return --c;
+        }
+        System.out.println("error");
+        return -1;        
     }
     
     
